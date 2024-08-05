a98ae4c9f26c87856e43a45cb62953000e2e3122
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-05 18:44:09 -0400

    add BinarySearch

---
+++ BinarySearch.ts
@@ -0,0 +1,15 @@
+function search(nums: number[], target: number): number {
+    let lo: number = 0;
+    let hi: number = nums.length-1;
+
+    while(lo<=hi){
+        let m=Math.floor((lo+hi)/2)
+        if(nums[m]==target){
+            return m
+        }else if(target<nums[m]){
+            hi=m-1
+        }else lo = m+1
+    }
+    return -1;
+
+};

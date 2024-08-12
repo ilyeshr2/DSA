73da11ddd176da6cb2ada1b5ffe0b55a7b9435d0
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-12 12:46:09 -0400

    Create deleteDuplicates.ts

---
+++ Linked_lists/deleteDuplicates.ts
@@ -0,0 +1,12 @@
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let curr=head
+
+    while(curr){
+        if(curr.val===curr.next?.val){
+            curr.next=curr.next.next
+        }else{
+            curr=curr.next
+        }
+    }
+    return head
+};

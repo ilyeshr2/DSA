7fe50ee33e37d1e916db141a77dd7da48c296a85
ilyeshr2 <ilyesharrache0@gmail.com>
2024-08-14 18:08:51 +0100

    Create 206-reverseList.ts

---
+++ Linked_lists/206-reverseList.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null
+    let curr = head 
+    let next
+
+    while(curr){
+        next=curr?.next
+        curr.next=prev
+        prev=curr
+        curr=next
+    }
+    head = prev
+    return  head
+};
